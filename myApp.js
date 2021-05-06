fec0369c6b6ca9f4fb8d6e9d3280b9d5e7bd941b
beka-bat1 <bekabatmanashvili1@gmail.com>
2021-05-06 19:11:37 +0400

    Create myApp.js

---
+++ myApp.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+
+let absolutePath = __dirname + "/";
+
+express.static("/", absolutePath);
+
+app.get("/api/timestamp", (res, req) => {
+  console.log(req.path);
+
+  res.json({ unix: Date.now(), utc: Date() });
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  if (/\d{5,}/.test(dateString)) {
+    const dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  } else {
+    let dateObject = new Date(dateString);
+
+    if (dateObject.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+    }
+  }
+});
