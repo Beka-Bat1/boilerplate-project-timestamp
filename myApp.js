d2957f04e34e73af7bc8e7e53ef1255aafbd6420
beka-bat1 <bekabatmanashvili1@gmail.com>
2021-05-07 16:38:25 +0400

    first api ready;

--- myApp.js
+++ myApp.js
@@ -1,29 +1,19 @@
-var express = require("express");
-var app = express();
 
-// let absolutePath = __dirname + "/";
 
-app.use("/", express.static(__dirname + "/"))
+// app.get("/api/timestamp/:date_string", (req, res) => {
+//   let dateString = req.params.date_string;
 
-console.log(new Date(Date.UTC(0)))
-app.get("/api/timestamp", (req, res) => {
-  res.json({ unix: new Date.now(), utc: new Date() });
-});
+//   if (/\d{5,}/.test(dateString)) {
+//     const dateInt = parseInt(dateString);
 
-app.get("/api/timestamp/:date_string", (req, res) => {
-  let dateString = req.params.date_string;
+//     res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+//   } else {
+//     let dateObject = new Date(dateString);
 
-  if (/\d{5,}/.test(dateString)) {
-    const dateInt = parseInt(dateString);
-
-    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
-  } else {
-    let dateObject = new Date(dateString);
-
-    if (dateObject.toString() === "Invalid Date") {
-      res.json({ error: "Invalid Date" });
-    } else {
-      res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
-    }
-  }
-});
+//     if (dateObject.toString() === "Invalid Date") {
+//       res.json({ error: "Invalid Date" });
+//     } else {
+//       res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+//     }
+//   }
+// });
--- server.js
+++ server.js
@@ -2,31 +2,43 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
+//trying to make static port
+var port = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// your first API endpoint...
+app.get("/api/:date?", function (req, res) {
+  let timeStamp = req.params.date;
+  console.log(timeStamp)
+  // checking if timeStamp is numbers or 2015-12-15
+  timeStamp.length >= 12 ? timeStamp=parseInt(timeStamp) : null ;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  let calculatedDate = new Date(timeStamp);
+  if (calculatedDate == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    let utcDate = calculatedDate.toUTCString();
+    let message = { unix: calculatedDate.getTime(), utc: utcDate };
+    res.json(message);
+  }
 });
-
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(port, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
